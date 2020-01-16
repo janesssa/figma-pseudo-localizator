3d7126ac4e7aae9bb1a53290abbdea1f149f0b2c
Janessa Labeur <labeur@redkiwi.nl>
2020-01-16 10:41:15 +0100

    [TASK] Removed unnecassary buttons

--- code.ts
+++ code.ts
@@ -1,9 +1,10 @@
-figma.showUI(__html__, { width: 320, height: 240});
+figma.showUI(__html__, { width: 320, height: 180});
 
 const cachedNodes = {}
 const originalNodes = {}
+const nodes = {}
 const newNodes = {}
-let reverted = false
+let original = false 
 
 const getTextNodes = (selection, percentage) => {
   let id = 0;
@@ -15,31 +16,33 @@ const getTextNodes = (selection, percentage) => {
     }
     else {
       if (node.type === 'TEXT' && node.characters !== undefined && node.characters.length < 100) {
+        if(!original){
+          originalNodes[id] = { ...node, characters: node.characters }
+          original = true
+        }
         cachedNodes[id] = { node: node }
-        originalNodes[id] = { ...node, characters: node.characters, fontName: node.fontName }
+        nodes[id] = { ...node, characters: node.characters, fontName: node.fontName}
         id++
       }
     }
   }
   selection.forEach(item => childrenIterator(item))
 
-  startProgress('start')
-  stopProgress('revert')
-  changeText({...originalNodes}, percentage)
+  changeText(nodes, percentage)
 }
 
 const changeText = (node, percentage) => {
   const extraText = 'DoNec A VeSTIBuLUM erat. mAuRis sED Orci loREm. DoNEc mAXImUs LAciNiA poSUERE. mAURis diCtuM, PuRus qUiS ALIqUAM vehicULA, ArCu loREm iNTErDUM ElIt, A ConsEctEtUr NullA NISI Et QUaM. FUScE lOBorTIS VOLuTPAT fRinGILLa. PrOIN alIqUEt aT iPSuM sagitTIs CoNdimeNTUm. vIvamUS NEc ANte EU tuRPIS VArIUS ALIquEt. NuLLa EUiSmOd loBOrTiS ARCu, Et PHaRETRA ENIm SEMPEr Eu. PrOin In FrinGilla aRcu, qUis FeuGiAt dUI. suspendiSsE LucTus pOrtA nIsL, NeC VEHiCULa lOreM TeMpUS aT. inTeRduM Et mALesuadA fAmeS ac AnTE IPsum PRIMIS iN FAUCibUs. prAeseNT odIO DOlor, cuRsus EU fauCibuS vEL, VulPutaTE In DoLor. inTeger maxIMUs loreM VOLUTPAt GrAVIDa ullAmCOrPeR. PhaSELlUs EU fELis ARCu. nULLaM VolUtPAt NUnc TiNCiDuNT VehicuLa varIus. aeNeaN ImPErDiet qUAM Eget DuI ORnarE PELleNTesQUe. qUIsque Semper leo AT phaReTrA IaCUlis. sUSPEnDIsSe RutruM, LectUs ac PHAreTRA FRInGiLLA, NUlLA leo FInIBus UrNA, Ut frINgiLla NibH DuI ULTRIceS mEtuS. pellENtEsQue vitAE VArIUs neQUE. ViVaMus fRInGILla Ex SEd est gRaviDA consEquAt. dUis matTIS UT Mi seD DiGNiSSIm. PellenTESqUE viTae NISi SiT amET ODIo sEmPeR voLUtPaT. dUis scElERiSque, FElIs A alIQuEt CondImeNtUM, meTUs enIM sCeLErISQUe ODiO, PULVINAr iMPErDIeT pURus NunC iN leCtUs. CUrAbITUr Sit amET CUrSUS lorEM, IN HEndReRIt ODio. PHaSELluS VOLUtpat posuere QuAm Ut BlAndIT. VEstIBuLum PORTtitOR RISUS LeO, Ac SCelerIsQUe SEm lUCtUs NOn. moRBi QUiS Ante ut DoLor UltRiciEs GrAVIda EGet eGet TellUs. aLIQUam DiCtuM vEhiculA TURPIs, ET BLAndIT toRtoR tINcIDunT Sed. eTIAM TiNcIdUnt moLeSTIE EnIm UT cOnVAllIS. nuLlaM EFFiCituR vestiBuLUM TOrtOr, ac inTErDum auguE TiNciDUnT eGet. Ut cOnDImenTum Nisl ENIm, aT ViveRra NulLa CoNGuE A. PeLlENteSQUe EU EfficItUR auGUe.'
   let id = -1
   Object.keys(node).forEach(async (key) => {
-    let characters = node[key].characters
+    let characters = originalNodes[key].characters
     let fontName = node[key].fontName
     let uid = node[key].id
     const length = Math.ceil(characters.length * percentage) 
     const random = Math.ceil(Math.random() * (extraText.length - length)) 
-    const addText = extraText.slice(random, random + length)
+    let addText = extraText.slice(random, random + length)
+    if(addText.slice(0, 1) === ' ') addText = extraText.slice(random + 1, random + length + 1)
     const newText = pseudoText(addText)
-    debugger
     if (fontName != figma.mixed) {
       await figma.loadFontAsync(fontName).then(() => {
         id++
@@ -122,38 +125,19 @@ const revertText = (key, obj) => {
   cachedNodes[key].node.characters = obj[key].characters
 }
 
-const startProgress = (tag) => {
-  let message = { type: 'start', options: { tag: tag } }
-  figma.ui.postMessage(message)
-}
-
-const blinkButton = (tag) => {
-  let message = { type: 'blink', options: { tag: tag } }
-  figma.ui.postMessage(message)
-}
-
-const stopProgress = (tag) => {
-  let message = { type: 'stop', options: { tag: tag } }
-  figma.ui.postMessage(message)
-}
-
 figma.ui.onmessage = msg => {
   switch (msg.type) {
     case 'start': {
       let percentage = msg.options.input / 100
       getTextNodes(figma.currentPage.children, percentage)
-      reverted = false
       break
     }
     case 'revert': {
       let alertOnce = false
       let message = 'Caution! There is nothing to revert.'
-      reverted = true
       if(Object.keys(originalNodes).length === 0 || Object.keys(newNodes).length === 0){
         alert(message)
       } else {
-        startProgress('revert')
-        stopProgress('start')
         Object.keys(originalNodes).forEach(key => {
           if(originalNodes[key].characters === newNodes[key].characters){
             if(!alertOnce){
@@ -167,22 +151,11 @@ figma.ui.onmessage = msg => {
       }
       break
     }
-    case 'cancel': {
-      let alertOnce = false
-      stopProgress('revert')
-      stopProgress('start')
-      debugger
-      Object.keys(originalNodes).forEach(key => {
-        if(!reverted){
-          if(!alertOnce){
-            blinkButton('revert')
-            alert('Caution! Your text is not reverted yet.')
-            alertOnce = true
-          } 
-        } else {
-          figma.closePlugin()
-        }
-      })
-    }
   }
 }
+
+figma.on('close', () => {
+  Object.keys(originalNodes).forEach(key => {
+    revertText(key, originalNodes)
+  })
+})
